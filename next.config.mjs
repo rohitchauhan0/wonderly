4628e22f48cb0ba182ad7203cf13f54335d92576
rohitchauhan0 <beatfeelrc121@gmail.com>
2024-09-27 19:57:39 +0530

    fourth commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.pexels.com', 'media.licdn.com'],
+        domains: ['images.pexels.com', 'media.licdn.com', 'ajay-portfolio-theta.vercel.app','plus.unsplash.com'],
     },
     webpack(config) {
       // Add a rule to handle audio files
