cb5ce12bb45f65dd2028a9705320f98af40afa74
rohitchauhan0 <beatfeelrc121@gmail.com>
2024-09-28 12:38:00 +0530

    eigth commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 "use client";
 import { Facebook, Link, Linkedin, Twitter } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 const Footer = () => {
@@ -7,8 +8,11 @@ const Footer = () => {
     <footer className=" bg-gradient-to-t from-yellow-500 via-yellow-700 to-black text-white py-8">
       <div className="container mx-auto px-4 grid grid-cols-1 md:grid-cols-4 gap-8">
         <div>
-          <h3 className="text-xl font-bold mb-4">XYZ Company</h3>
-          <p className="text-gray-400">
+          <div className="mb-4 flex items-center space-x-5">
+            <Image src="/logo.png" alt="Logo" width={200} height={200} className=" h-20 w-20 rounded-full" />
+          <h3 className="text-xl font-bold mb-4">Wonderly</h3>
+          </div>
+          <p className="text-gray-400 rounded-t-sm">
             We offer amazing travel packages and experiences across the world.
             Explore, discover, and embark on unforgettable journeys with us.
           </p>
